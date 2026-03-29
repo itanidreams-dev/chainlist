8fced3412ba47ac42c04a1d5825c71ac0ebc8c44
itanidreams-dev <itanidreams@gmail.com>
2026-03-29 19:41:16 +0200

    feat: update iTani mainnet registry and icon slug

--- constants/additionalChainRegistry/chainid-1229800785.js
+++ constants/additionalChainRegistry/chainid-1229800785.js
@@ -2,7 +2,11 @@ export const data = {
   "name": "iTani Network Chain",
   "chain": "ITANI",
   "rpc": [
-    "https://itani-network-chain-kirdnwz4rq-uc.a.run.app/"
+    "https://node.itaninetwork.chain/jsonrpc",
+    "https://itani-mainnet-1009642477948.us-central1.run.app/jsonrpc",
+    "https://itani-network-chain-1009642477948.us-central1.run.app/jsonrpc",
+    "https://itani-network-chain-1009642477948.europe-west9.run.app/jsonrpc",
+    "https://itani-network-chain-kirdnwz4rq-uc.a.run.app/jsonrpc"
   ],
   "faucets": [],
   "nativeCurrency": {
@@ -11,10 +15,17 @@ export const data = {
     "decimals": 18
   },
   "features": [{ "name": "EIP155" }],
-  "infoURL": "https://github.com/iTaniCore/iTani-Network-Chain",
+  "infoURL": "https://github.com/itanidreams-dev/iTani-Network-Chain",
   "shortName": "itani",
+  "chainSlug": "itani",
   "chainId": 1229800785,
   "networkId": 1229800785,
   "icon": "itani",
-  "explorers": []
+  "explorers": [
+    {
+      "name": "iTani Explorer",
+      "url": "https://node.itaninetwork.chain",
+      "standard": "EIP3091"
+    }
+  ]
 }
