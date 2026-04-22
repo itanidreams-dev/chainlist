16ef7b2014b1ff67f32d1a8d9526b0919f101aad
iTani <itanidreams@gmail.com>
2026-04-22 20:35:15 +0200

    Update iTani Network Chain configuration
    iTani Network Chain Website Url

--- constants/additionalChainRegistry/chainid-1229800785.js
+++ constants/additionalChainRegistry/chainid-1229800785.js
@@ -1,20 +1,38 @@
 export const data = {
-  "name": "iTani Network Chain",
-  "chain": "ITANI",
-  "rpc": [
-    "https://itani-network-chain-kirdnwz4rq-uc.a.run.app/"
+  name: "iTani Network Chain",
+  chain: "ITN",
+  icon: "itani",
+  rpc: [
+    "https://node.itaninetworkchain.com/jsonrpc",
+    "https://relay.itaninetworkchain.com/jsonrpc"
   ],
-  "faucets": [],
-  "nativeCurrency": {
-    "name": "iTani",
-    "symbol": "ITANI",
-    "decimals": 18
+  faucets: [],
+  nativeCurrency: {
+    name: "iTani",
+    symbol: "ITANI",
+    decimals: 18
   },
-  "features": [{ "name": "EIP155" }],
-  "infoURL": "https://github.com/iTaniCore/iTani-Network-Chain",
-  "shortName": "itani",
-  "chainId": 1229800785,
-  "networkId": 1229800785,
-  "icon": "itani",
-  "explorers": []
-}
+  features: [{ name: "EIP155" }],
+  infoURL: "https://www.itaninetworkchain.com",
+  shortName: "itani",
+  chainId: 1229800785,
+  networkId: 1229800785,
+  explorers: [
+    {
+      name: "iTani Explorer",
+      url: "https://node.itaninetworkchain.com",
+      standard: "EIP3091"
+    },
+    {
+      name: "iTani Explorer (Relay)",
+      url: "https://relay.itaninetworkchain.com",
+      standard: "EIP3091"
+    },
+  ,
+    {
+      name: "iTani Website (www)",
+      url: "https://www.itaninetworkchain.com",
+      standard: "HTML"
+    }
+  ]
+};
